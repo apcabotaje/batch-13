4ec5c7bb89b0dc0f4af6183dbff35d94e430ec8a
apcabotaje <123766957+ayman-drtalks@users.noreply.github.com>
2024-12-13 20:58:09 +0800

    initial commit

---
+++ assets/scripts/main.js
@@ -0,0 +1,8 @@
+const div = document.querySelector("div")
+const spanHi = document.querySelector("#hi")
+const spanBye = document.querySelector("#bye")
+
+
+spanHi.style.color = "red"
+spanBye.style.backgroundColor = "blue"
+
